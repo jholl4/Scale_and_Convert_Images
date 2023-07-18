57197869e89ffa4b79557945f3552df2a0b78b8c
Joshua Holland <joshua.holland.1@outlook.com>
2023-07-17 20:52:09 -0400

    Updated code and it's now functional but black images now show up as a black square.

--- Scale_and_Convert_Images.py
+++ Scale_and_Convert_Images.py
@@ -18,18 +18,28 @@ def pic_converter():
     # Counter for how many pictures have been processed
     counter = 0
 
+    # Make the destination directory
+    try:
+        os.makedirs("./opt/icons")
+    except:
+        print("directory already exists")
+    
+
     # Iterate over pictures in folder
     for img  in imgs:
         try:
             with Image.open(src + img) as im:
-                im.resize(size).rotate(-90).save(dst + img, 'JPEG')
+                im = im.convert('RGB')
+                im = im.resize(size)
+                im = im.rotate(270)
+                im.save(dst + img, 'JPEG')
                 # Increment the counter
                 counter += 1
         except OSError:
             print("Cannot convert" + img)
 
     # Print the total amount of pictures processed
-    print("{} pictures rotated 90 degrees clockwise, resized to 128x128 and saved in /opt/icons/".format(counter))
+    print("{} pictures rotated 90 degrees clockwise, resized to 128x128 and saved in ./opt/icons/".format(counter))
     
 
 if __name__ == "__main__":
